374ec853781208dbe7037d77aa7bc71a7458d14b
Jonathan Lam <jonlam1989@outlook.com>
2023-02-24 13:26:38 -0500

    added 2 dropdown menus to display stats: for transaction_type + state

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -10,7 +10,7 @@ from dash import Dash, dash_table, dcc, html, Input, Output
 
 # read cleaned data + filter for only the SSN (need this to merge dataframes) and CUST_ZIP
 customer_df = pd.read_csv('cleaned_files/cleaned_customer.csv')
-customer_df = customer_df[['SSN', 'CUST_ZIP']]
+customer_df = customer_df[['SSN', 'CUST_ZIP', 'CUST_STATE']]
 
 # read cleaned data + rename column (need this to merge dataframes)
 credit_card_df = pd.read_csv('cleaned_files/cleaned_credit.csv')
@@ -24,6 +24,14 @@ merged_df.drop(columns=['SSN'], inplace=True)
 merged_df['TIMEID'] = pd.to_datetime(merged_df['TIMEID'], format='%Y%m%d')
 # keep only the date (not the timezone)
 merged_df['TIMEID'] = merged_df['TIMEID'].dt.date 
+
+# dropdown label
+transaction_type = merged_df['TRANSACTION_TYPE'].unique()
+transaction_type_options = [{'label': type, 'value': type} for type in transaction_type]
+# state label
+state = merged_df['CUST_STATE'].unique()
+state.sort()
+state_options = [{'label': s, 'value': s} for s in state]
 
 # find unique zipcodes
 list_of_zipcodes = merged_df['CUST_ZIP'].sort_values().unique()
@@ -41,11 +49,10 @@ years_df = pd.DataFrame(list_of_years, columns=['Filter by Year'])
 app = Dash(__name__)
 
 app.layout = html.Main([
-                html.Section([
+                html.Div([
                     html.H1('Customer Transactions'),
-                    html.Br(),
                     html.Div([
-                        html.Div([
+                        html.Section([
                             dash_table.DataTable(merged_df.to_dict('records'),                              # https://dash.plotly.com/datatable
                                                 [{'name': i, 'id': i} for i in merged_df.columns], 
                                                 page_size=10, 
@@ -53,8 +60,46 @@ app.layout = html.Main([
                                                 style_as_list_view=True,
                                                 style_header={'fontWeight': 'bold'},
                                                 style_cell={'textAlign': 'center', 'font-family': 'Sans-serif'}),
+                            html.Div([
+                                html.Section([
+                                    html.Div([
+                                        html.Section([
+                                            html.P('Transactions'),
+                                            html.P('0')
+                                        ]),
+                                        html.Section([
+                                            html.P('Total Dollars'),
+                                            html.P('$0')
+                                        ])
+                                    ], className='values'),
+                                    dcc.Dropdown(
+                                        id='transaction_type',
+                                        className='dropdown',
+                                        placeholder='Select a transaction type',
+                                        options=transaction_type_options,
+                                        maxHeight=100
+                                    )], className='values_container'),
+                                html.Section([
+                                    html.Div([
+                                        html.Section([
+                                            html.P('Branches'),
+                                            html.P('0')
+                                        ]),
+                                        html.Section([
+                                            html.P('Total Dollars'),
+                                            html.P('$0')
+                                        ])
+                                    ], className='values'),
+                                    dcc.Dropdown(
+                                        id='state',
+                                        className='dropdown',
+                                        placeholder='Select a state',
+                                        options=state_options,
+                                        maxHeight=100
+                                    )], className='values_container')
+                            ], className='stats')
                         ], className='data'),
-                        html.Div([
+                        html.Aside([
                             dash_table.DataTable(zipcodes_df.to_dict('records'), 
                                                  [{'name': i, 'id': i} for i in zipcodes_df],
                                                  id='zipcode_list',
@@ -74,7 +119,7 @@ app.layout = html.Main([
                                                  style_table={'height': '150px','overflowY': 'auto'})
                         ], className='data_sidebar')
                     ], className='container')
-                ], className='section')
+                ], className='main_container')
             ], className='main')
 #----------------------------------------------------------------------------------------------------------
 # update dashboard based on user input
@@ -86,10 +131,10 @@ def update_data_table(zipcode_list, months_list, years_list):
     if zipcode_list == None and months_list == None and years_list == None:
         return merged_df.to_dict('records')
     else:
-        # defaults: if end-user does not click on any filters
-        zipcode_target = merged_df['CUST_ZIP']
-        month_target = merged_df['TIMEID']
-        year_target = merged_df['TIMEID']
+        # defaults to all, if end-user does not click on any filters 
+        zipcode_target = merged_df['CUST_ZIP'] != None
+        month_target = merged_df['TIMEID'] != None
+        year_target = merged_df['TIMEID'] != None
 
         # filter by zipcode 
         if zipcode_list:
@@ -110,6 +155,10 @@ def update_data_table(zipcode_list, months_list, years_list):
         if months_list and years_list:
             common_target = month_target.isin(year_target)
             filtered_df = merged_df[zipcode_target & common_target]
+        elif months_list == None and years_list:
+            filtered_df = merged_df[zipcode_target & year_target]
+        elif months_list and years_list == None:
+            filtered_df = merged_df[zipcode_target & month_target]
         else:
             filtered_df = merged_df[zipcode_target & month_target & year_target]
         # sort by day in descending order

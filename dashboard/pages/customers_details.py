34498f69e20f58e1641cedc3d572d7d7472520af
Jonathan Lam <jonlam1989@outlook.com>
2023-03-01 13:39:48 -0500

    refactored form component: callback error-nonexistant object used in Input

--- dashboard/pages/customers_details.py
+++ dashboard/pages/customers_details.py
@@ -167,7 +167,21 @@ layout = html.Main([
                             style_as_list_view=True,
                             style_header={'fontWeight': 'bold'},
                             style_cell={'textAlign': 'center', 'font-family': 'Sans-serif'}),
-        html.Form(id='edit_form', style={'display': None}),
+        html.Form([
+            html.H2('Edit the Existing Customer Details'),
+            dcc.Input(id='edit_first', type='text', value='', placeholder='Edit First Name', debounce=True), 
+            dcc.Input(id='edit_mid', type='text', value='', placeholder='Edit Middle Name', debounce=True),
+            dcc.Input(id='edit_last', type='text', value='', placeholder='Edit Last Name', debounce=True),
+            dcc.Input(id='edit_cc', type='text', value='', placeholder='Edit Credit Card', debounce=True),
+            dcc.Input(id='edit_street', type='text', value='', placeholder='Edit Street Address', debounce=True),
+            dcc.Input(id='edit_city', type='text', value='', placeholder='Edit City', debounce=True),
+            dcc.Input(id='edit_state', type='text', value='', placeholder='Edit State', debounce=True),
+            dcc.Input(id='edit_country', type='text', value='', placeholder='Edit Country', debounce=True),
+            dcc.Input(id='edit_zip', type='text', value='', placeholder='Edit Zip Code', debounce=True),
+            dcc.Input(id='edit_phone', type='text', value='', placeholder='Edit Phone', debounce=True),
+            dcc.Input(id='edit_email', type='text', value='', placeholder='Edit Email', debounce=True),
+            html.Div(html.Button('Submit', id='Submit', n_clicks=0))
+        ], id='edit_form', style={'display': 'none'}),                                              # hide the form
         html.Div(id='output'),
         html.H2('Customer Transactions', id='transactions_header'),
         html.Div([
@@ -186,8 +200,11 @@ layout = html.Main([
 #----------------------------------------------------------------------------------------------------------
 # update customer details based on user input - *NEED TO CONVERT @app.callback -> @dash.callback FOR MULTI-PAGE FUNCTIONALITY*
 @dash.callback(
-    [Output('error', 'children'), Output('details', 'data'), Output('transaction', 'data'), Output('edit_form', 'children'),
-     Output('data_store', 'data')],
+    [Output('error', 'children'), Output('details', 'data'), Output('transaction', 'data'), Output('data_store', 'data'),
+     Output('edit_form', component_property='style'), Output('edit_first', 'value'), Output('edit_mid', 'value'), 
+     Output('edit_last', 'value'), Output('edit_cc', 'value'), Output('edit_street', 'value'), 
+     Output('edit_city', 'value'), Output('edit_state', 'value'), Output('edit_country', 'value'), 
+     Output('edit_zip', 'value'), Output('edit_phone', 'value'), Output('edit_email', 'value')],
     [Input('first', 'value'), Input('middle', 'value'), Input('last', 'value'), 
      Input('date_range', 'start_date'), Input('date_range', 'end_date')]
 )
@@ -200,10 +217,9 @@ def update_details(first, middle, last, start_date, end_date):
 
         # find customer based on full name in customer_df
         target_customer_df = customer_df[first_name & middle_name & last_name]
-        
         # print error message if dataframe is empty=(name is NOT found)
         if target_customer_df.empty:
-            return ['(No customer found with this name...please try again)', None, None, None, None]
+            return ['(No customer found with this name...please try again)', None, None, None, {'display':'none'}, '', '', '', '', '', '', '', '', '', '', '']
         else:
             # find customer based on ssn in credit_card_df
             ssn = target_customer_df['SSN'].values[0]
@@ -224,28 +240,25 @@ def update_details(first, middle, last, start_date, end_date):
 
             target_transactions_df['TIMEID'] = credit_card_df['TIMEID'].dt.date
 
-            # return customer details + customer transactions if name is found
-            return ['', target_customer_df.to_dict('records'), target_transactions_df.to_dict('records'), 
-                    [
-                        html.H2('Edit the Existing Customer Details'),
-                        dcc.Input(id='edit_first', type='text', value=target_customer_df['FIRST_NAME'].values[0], placeholder='Edit First Name', debounce=True), 
-                        dcc.Input(id='edit_mid', type='text', value=target_customer_df['MIDDLE_NAME'].values[0], placeholder='Edit Middle Name', debounce=True),
-                        dcc.Input(id='edit_last', type='text', value=target_customer_df['LAST_NAME'].values[0], placeholder='Edit Last Name', debounce=True),
-                        dcc.Input(id='edit_cc', type='text', value=target_customer_df['CREDIT_CARD_NO'].values[0], placeholder='Edit Credit Card', debounce=True),
-                        dcc.Input(id='edit_street', type='text', value=target_customer_df['FULL_STREET_ADDRESS'].values[0], placeholder='Edit Street Address', debounce=True),
-                        dcc.Input(id='edit_city', type='text', value=target_customer_df['CITY'].values[0], placeholder='Edit City', debounce=True),
-                        dcc.Input(id='edit_state', type='text', value=target_customer_df['STATE'].values[0], placeholder='Edit State', debounce=True),
-                        dcc.Input(id='edit_country', type='text', value=target_customer_df['COUNTRY'].values[0], placeholder='Edit Country', debounce=True),
-                        dcc.Input(id='edit_zip', type='text', value=target_customer_df['ZIP'].values[0], placeholder='Edit Zip Code', debounce=True),
-                        dcc.Input(id='edit_phone', type='text', value=target_customer_df['PHONE'].values[0], placeholder='Edit Phone', debounce=True),
-                        dcc.Input(id='edit_email', type='text', value=target_customer_df['EMAIL'].values[0], placeholder='Edit Email', debounce=True),
-                        html.Div(html.Button('Submit', id='Submit', n_clicks=0))
-                    ],
-                    ssn
+            # return customer details + customer transactions + ssn if name is found
+            # pre-populate form with current customer details
+            return ['', target_customer_df.to_dict('records'), target_transactions_df.to_dict('records'), ssn,
+                    {'display': 'block'},                                                           # show the form
+                    target_customer_df['FIRST_NAME'].values[0], 
+                    target_customer_df['MIDDLE_NAME'].values[0], 
+                    target_customer_df['LAST_NAME'].values[0],
+                    target_customer_df['CREDIT_CARD_NO'].values[0], 
+                    target_customer_df['FULL_STREET_ADDRESS'].values[0], 
+                    target_customer_df['CITY'].values[0], 
+                    target_customer_df['STATE'].values[0],
+                    target_customer_df['COUNTRY'].values[0], 
+                    target_customer_df['ZIP'].values[0], 
+                    target_customer_df['PHONE'].values[0], 
+                    target_customer_df['EMAIL'].values[0] 
             ]
     else:
         # defaults - when the page first loads 
-        return ['', None, None, None, None]
+        return ['', None, None, None, {'display': 'none'}, '', '', '', '', '', '', '', '', '', '', '']
 #----------------------------------------------------------------------------------------------------------
 # update customer details based on user input - *NEED TO CONVERT @app.callback -> @dash.callback FOR MULTI-PAGE FUNCTIONALITY*
 @dash.callback(
